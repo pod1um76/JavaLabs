0bc7a33fec675fb6e9eca2715e772351c95f9a6b
antonlyn77 <lyndin2001@mail.ru>
2020-10-24 00:51:30 +0300

    Completion Lab12

--- Lab12/src/Money.java
+++ Lab12/src/Money.java
@@ -1,10 +1,12 @@
 public class Money {
+    static final int PATH_VASA = 2;
+    static final int PATH_MASHA = 11;
 
     void children() {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
         String[] Parts = text.split(" ");
-        int moneyVasiliy = Integer.parseInt(Parts[2].trim());
-        int moneyMasha = Integer.parseInt(Parts[11].trim());
+        int moneyVasiliy = Integer.parseInt(Parts[PATH_VASA].trim());
+        int moneyMasha = Integer.parseInt(Parts[PATH_MASHA].trim());
         System.out.println("Cумма денег Василия и Марии: " + (moneyVasiliy + moneyMasha));
     }
 }
