d051621fd49681d10a97bd9b34aa938d378fb3d9
antonlyn77 <lyndin2001@mail.ru>
2020-10-23 23:56:06 +0300

    Init

--- Lab13/src/Personality.java
+++ Lab13/src/Personality.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
 public class Personality {
+    static final int MIN_FULL_NAME_LENGTH = 2;
+
+    private boolean isCyrillic(String str) {
+        for (char i : str.toCharArray()) {
+            if (i != ' ' && i != '-' && (i < 'А' || i > 'я')) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public void data() {
         Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-        String[] name_parts = name.split(" ");
+        String fullName = in.nextLine();
+        String[] nameParts = fullName.split(" ");
 
-        if (name_parts.length < 2) {
+
+        if (!isCyrillic(fullName) || nameParts.length < MIN_FULL_NAME_LENGTH) {
             System.out.println("Введенная строка не является ФИО");
             return;
         }
-        for (char i : name.toCharArray()) {
-            if (i != ' ' && i != '-' && (i < 'А' || i > 'я')) {
-                System.out.println("Введенная строка не является ФИО");
-                return;
-            }
-        }
-
-        System.out.println("Фамилия: " + name_parts[0]);
+        System.out.println("Фамилия: " + nameParts[0]);
+        System.out.println("Имя: " + nameParts[1]);
+        if (nameParts.length > MIN_FULL_NAME_LENGTH)
+            System.out.println("Отчество: " + nameParts[2]);
     }
 }
 class Test {
