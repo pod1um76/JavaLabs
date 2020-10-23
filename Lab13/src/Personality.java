8ee8b49e5fd908780bc7d1600275ecac28cdba60
antonlyn77 <lyndin2001@mail.ru>
2020-10-23 23:39:27 +0300

    Init

--- Lab13/src/Personality.java
+++ Lab13/src/Personality.java
@@ -1,48 +1,24 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-
-import static java.lang.System.*;
 
 public class Personality {
 
     public void data() {
         Scanner in = new Scanner(System.in);
-        String smMn;
-        smMn = in.next();
-        int counter = 0, index = 0;
-        for (int i = 0; i < smMn.length(); i++) {
-            if (smMn.charAt(i) == 0) {
-                counter++;
-            }
-            if (smMn.charAt(i) != 45 && (smMn.charAt(i) < 192|| smMn.charAt(i) > 255)) {
-                out.println("Введенная строка не является ФИО");
-                break;
+        String name = in.nextLine();
+        String[] name_parts = name.split(" ");
+
+        if (name_parts.length < 2) {
+            System.out.println("Введенная строка не является ФИО");
+            return;
+        }
+        for (char i : name.toCharArray()) {
+            if (i != ' ' && i != '-' && (i < 'А' || i > 'я')) {
+                System.out.println("Введенная строка не является ФИО");
+                return;
             }
         }
-        if (counter == 0) {
-            out.println("Введенная строка не является ФИО");
-        }
-        else {
-            out.print("Фамилия: ");
-            while (smMn.charAt(index) != 0) {
-                out.print(smMn.charAt(index));
-                index++;
-            }
-            index++;
-            out.print("\nИмя: ");
-            while (smMn.charAt(index) != 0 || index != smMn.length()) {
-                out.print(smMn.charAt(index));
-                index++;
-            }
-            if (index != smMn.length()) {
-                index++;
-                out.print("\nОтчество: ");
-                while (smMn.charAt(index) != 0 || index != smMn.length()) {
-                    out.print(smMn.charAt(index));
-                    index++;
-                }
-            }
-        }
+
+        System.out.println("Фамилия: " + name_parts[0]);
     }
 }
 class Test {
