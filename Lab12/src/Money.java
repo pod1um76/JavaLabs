63bcd20741b5f92fb7b559703c06a9c12e00f94e
antonlyn77 <lyndin2001@mail.ru>
2020-10-24 00:30:57 +0300

    Completion Lab12

--- Lab12/src/Money.java
+++ Lab12/src/Money.java
@@ -1,13 +1,18 @@
 public class Money {
-    void children() {
-        String moneyVasa, moneyMasha;
-        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        for (int i = 0; i < text.length(); i++) {
 
-        }
+    void children() {
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String[] Parts = text.split(" ");
+        int moneyVasiliy = Integer.parseInt(Parts[2].trim());
+        int moneyMasha = Integer.parseInt(Parts[11].trim());
+        System.out.println("Cумма денег Василия и Марии: " + (moneyVasiliy + moneyMasha));
     }
+}
+
+class TestMoney {
 
     public static void main(String args[]) {
-
+        Money obj = new Money();
+        obj.children();
     }
 }
