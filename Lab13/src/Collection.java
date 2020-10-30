8ea8bc2365182bcc4edff1e4f9f5d12449985bfd
antonlyn77 <lyndin2001@mail.ru>
2020-10-30 13:06:03 +0300

    Lab13

--- Lab13/src/Collection.java
+++ Lab13/src/Collection.java
@@ -1,41 +1,110 @@
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Collection {
-
-    Map<Integer, String> map = new TreeMap<>();
+    static Map<String, String> map = new TreeMap<>();
 
     void installation() {
-        map.put(798561177, "Вася" );
-        map.put(798535789, "Петя" );
-        map.put(798591063, "Аркаша" );
-        map.put(798512314, "Миша" );
-        map.put(798570056, "Гоша" );
-        map.put(798511128, "Коля" );
+        map.put("Вася", "798561177" );
+        map.put("Петя", "798535789" );
+        map.put("Аркаша", "798591063" );
+        map.put("Миша", "798512314" );
+        map.put("Гоша", "798570056" );
+        map.put("Коля", "798511128" );
     }
 
-    void addCollection() {
+    static void getCollection() {
+        for(Map.Entry<String, String> item : map.entrySet()){
 
+            System.out.println(item.getKey() +"-"+ item.getValue());
+        }
     }
 
+    static void setPhoneNumber(String key) {
+        System.out.println("Впишите номер: ");
+
+        Scanner in = new Scanner(System.in);
+        String phoneNumber = in.next();
+
+        map.put(key, phoneNumber);
+
+        getCollection();
+    }
+
+    static void setName(String value) {
+        System.out.println("Впишите имя: ");
+
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+
+        map.put(name, value);
+
+        getCollection();
+    }
+
+    void chekKey(String key) {
+        int counter = 0;
+
+        for (Map.Entry<String, String> item : map.entrySet()) {
+            if (key.equals(item.getKey())) {
+                System.out.println(item.getKey() +"-"+ item.getValue());
+                counter++;
+                break;
+            }
+        }
+
+        if (counter == 0)
+            setPhoneNumber(key);
+    }
+
+    void chekValue(String value) {
+        int counter = 0;
+        for(Map.Entry<String, String> item : map.entrySet()) {
+            if (value.equals(item.getValue())) {
+                System.out.println(item.getKey() + "-" + item.getValue());
+                counter++;
+                break;
+            }
+        }
+
+        if (counter == 0)
+            setName(value);
+    }
 }
 
 class TestCollection {
-
-    public static void main()  {
+    public static void main(String []args)  {
         Scanner in = new Scanner(System.in);
         Collection collection = new Collection();
-        String name;
-        int phoneNumber;
+        String command;
 
         collection.installation();
 
         boolean flag = true;
         while (flag) {
-            
+            command  = in.next();
 
+            boolean chekLetterLatin = command.matches("^[a-zA-Z0]+$");
+            boolean chekLetterCyrillic = command.matches("^[а-яА-Я0]+$");
+            boolean chekNumber = command.matches("\\-?\\d+(\\.\\d{0,})?");
+
+            switch (command) {
+                case "LIST":
+                    collection.getCollection();
+                    break;
+                case "STOP":
+                    flag = false;
+                    break;
+                default:
+                    if (chekNumber)
+                        collection.chekValue(command);
+                    else if (chekLetterLatin || chekLetterCyrillic)
+                        collection.chekKey(command);
+                    else
+                        System.out.println("Данные введены некорректно");
+                        break;
+            }
         }
     }
 }
