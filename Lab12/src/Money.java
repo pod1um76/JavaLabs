db1fbf2967ffd96fe3ccdcd4a48ea918580ae81b
antonlyn77 <lyndin2001@mail.ru>
2020-10-31 13:44:34 +0300

    Lab12

--- Lab12/src/Money.java
+++ Lab12/src/Money.java
@@ -1,19 +1,34 @@
+import java.util.StringTokenizer;
+
 public class Money {
-    static final int PATH_VASA = 2;
-    static final int PATH_MASHA = 11;
+    static final int PATH_VISA = 3;
+    static final int PATH_MASHA = 12;
 
     void children() {
+        int moneyVisa = 0;
+        int moneyMasha = 0;
+        int counter = 0;
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String[] Parts = text.split(" ");
-        int moneyVasiliy = Integer.parseInt(Parts[PATH_VASA].trim());
-        int moneyMasha = Integer.parseInt(Parts[PATH_MASHA].trim());
-        System.out.println("Cумма денег Василия и Марии: " + (moneyVasiliy + moneyMasha));
+        StringTokenizer st = new StringTokenizer(text, " ");
+
+        while(st.hasMoreTokens()) {
+            counter++;
+            String key = st.nextToken();
+
+            if (counter == PATH_VISA)
+                moneyVisa = Integer.parseInt(key.trim());
+
+            if (counter == PATH_MASHA)
+                moneyMasha = Integer.parseInt(key.trim());
+        }
+
+        System.out.println("Сумма денег Васи и Маши равна: " + (moneyVisa + moneyMasha) + " рублей");
     }
 }
 
 class TestMoney {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Money obj = new Money();
         obj.children();
     }
