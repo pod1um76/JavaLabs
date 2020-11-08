0a0dbf0c43eee93e3b7ce89e51d0cf16ff6dbd3e
antonlyn77 <lyndin2001@mail.ru>
2020-11-08 17:09:27 +0300

    correction

--- Lab07-8/src/Employee.java
+++ Lab07-8/src/Employee.java
@@ -1,60 +1,59 @@
-
-
 import java.util.*;
 
 public class Employee {
-    private String name, surname, position;
-    private double fixedSalary;
+    private final String name;
+    private final String surname;
+    private final String position;
+    private final double fixedSalary;
 
-    public Employee(String name, String surname, int fixedSalary, String position) {
+    public Employee(String name, String surname, double fixedSalary, String position) {
         this.name = name;
         this.surname = surname;
         this.fixedSalary = fixedSalary;
         this.position = position;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
+
     public String getName() {
         return name;
     }
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+
     public String getSurname() {
         return surname;
     }
-    public void setSalary(int fixedSalary) {
-        this.fixedSalary = fixedSalary;
-    }
+
     public double getSalary() {
         return fixedSalary;
     }
-    public void setPosition(String position) {
-        this.position = position;
-    }
+
     public String getPosition() {
         return position;
     }
 }
+
 interface EmployeePosition {
     String getJobTitle();
     double calcSalary();
 }
+
 class Manager implements EmployeePosition {
     Company company = new Company();
+
     public String getJobTitle() {
         return "Manager";
     }
+
     public double calcSalary() {
         return company.baseSalary + (((115000 + Math.random() * 140000) * 5) / 100);
     }
 }
+
 class TopManager implements EmployeePosition {
     Company company = new Company();
+
     public String getJobTitle() {
         return "TopManager";
     }
+
     public double calcSalary() {
         if (company.companyIncome > 10000000) {
             return company.baseSalary + ((company.baseSalary * 150) / 100);
@@ -62,74 +61,92 @@ class TopManager implements EmployeePosition {
         return  0;
     }
 }
+
 class Operator implements EmployeePosition {
     Company company = new Company();
+
     public String getJobTitle() {
         return "Operator";
     }
+
     public double calcSalary() {
         return company.baseSalary;
     }
 }
+
 class Company {
     public List<Employee> employees = new ArrayList<>();
-    int index;
-    public double baseSalary, companyIncome = 11000000;
-    public void hire(Employee employee) {
+    public double baseSalary = 5000;
+    public double companyIncome = 11000000;
+
+    public void hire(String position) {
         Manager manager = new Manager();
         TopManager topManager = new TopManager();
         Operator operator = new Operator();
-            if (employee.getPosition() == "Manager") {
-                employees.add(new Employee("Vasa", "Pupkin", (int) manager.calcSalary(), "Manager"));
-            }
-            else if (employee.getPosition() == "TopManager") {
-                employees.add(new Employee("Peta", "Kashkov", (int) topManager.calcSalary(), "TopManager"));
-            }
-            else if (employee.getPosition() == "Operator") {
-                employees.add(new Employee("Kola", "Petrov", (int) operator.calcSalary(), "Operator"));
-            }
+
+        switch (position) {
+            case "Manager":
+                employees.add(new Employee("Sam", "Crupper", manager.calcSalary(), manager.getJobTitle()));
+                break;
+            case "TopManager":
+                employees.add(new Employee("Dmitriy", "Shirking", topManager.calcSalary(), topManager.getJobTitle()));
+                break;
+            case "Operator":
+                employees.add(new Employee("Gennadi", "Krylov", operator.calcSalary(), operator.getJobTitle()));
+                break;
+        }
     }
+
     public void hireAll(String position, double numberEmployees ) {
         Manager manager = new Manager();
         TopManager topManager = new TopManager();
         Operator operator = new Operator();
-        for (index = 0; index < numberEmployees; index++) {
-            if (position == "Manager") {
-                employees.add(index, new Employee("Vasa", "Pupkin", (int) manager.calcSalary(), "Manager"));
-            }
-            else if (position == "TopManager") {
-                employees.add(index, new Employee("Peta", "Kashkov", (int) topManager.calcSalary(), "TopManager"));
-            }
-            else if (position == "Operator") {
-                employees.add(index, new Employee("Kola", "Petrov", (int) operator.calcSalary(), "Operator"));
+
+        for (int i = 0; i < numberEmployees; i++) {
+            switch (position) {
+                case "Manager":
+                    employees.add(i, new Employee("Jon", "Pushkin", manager.calcSalary(), manager.getJobTitle()));
+                    break;
+                case "TopManager":
+                    employees.add(i, new Employee("Peta", "Kharkov", topManager.calcSalary(), topManager.getJobTitle()));
+                    break;
+                case "Operator":
+                    employees.add(i, new Employee("Kola", "Petrov", operator.calcSalary(), operator.getJobTitle()));
+                    break;
             }
         }
     }
+
     public void fire() {
-        for (index = 0; index < (employees.size()) / 2; index++) {
-            employees.remove(index);
+        for (int i = 0; i < (employees.size()) / 2; i++) {
+            Employee remove = employees.remove(i);
         }
     }
+
     public double getIncome(double companyIncome) {
         return companyIncome;
     }
+
     public void show() {
         for (Employee i: employees) {
             System.out.println(i.getName() + " " + i.getSurname() + " " + i.getSalary() + " " + i.getPosition());
         }
     }
-    public void sortSalaryTop(){
-        Map<Employee, Double> mapSort = new TreeMap<Employee, Double>(new Comparator<Employee>() {
-            public int compare(Employee obj1, Employee obj2) {
-                return obj1.toString().compareTo(obj2.toString());
-            }
-        });
-        mapSort.putAll((Map<? extends Employee, ? extends Double>) employees);
-    }
-    public List<Employee> getLowestSalaryStaff(int count) {
-        if (count < 0 && count > employees.size()) {
-            return null;
+
+    public void getTopSalaryStuff(int count) {
+        if(count < 0 || count > employees.size()) {
+            return;
         }
+        ArrayList<Employee> top = new ArrayList<>(this.employees);
+        top.sort(Comparator.comparing(Employee::getSalary));
+        Collections.reverse(top);
+        ArrayList<Employee> countOfTop = new ArrayList<>();
+        for(int i = 0; i < count; i++) {
+            countOfTop.add( top.get(i) );
+        }
+    }
+
+    public void getLowestSalaryStaff(int count) {
         for (int i = 0; i < employees.size(); i++) {
             for (int j = 0; j < employees.size() - 1 - i; j++) {
                 if (employees.get(j).getSalary() > employees.get(j + 1).getSalary()) {
@@ -138,27 +155,38 @@ class Company {
                     employees.set(j, employees.get(j + 1));
                     employees.set(j + 1, t);
                 }
-
             }
         }
-        List<Employee> lowerSalary = new ArrayList<Employee>();
+        List<Employee> lowerSalary = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             lowerSalary.add(employees.get(i));
         }
-        return lowerSalary;
+    }
+
+    public int longList() {
+        return employees.size();
     }
 }
+
 class TestEmployee {
-    public static void main() {
-        int counter = 10;
+    public static void main(String []args) {
         Company company = new Company();
-        company.hireAll("Top Manager", 10);
-        company.hireAll("Manager", 80);
+        int count = company.longList();
+
+        System.out.println("Доход компании:" + company.getIncome(11000000));
+        company.hire("TopManager");
+        System.out.println("Найм сотрудника:");
+        company.show();
         company.hireAll("Operator", 180);
+        company.hireAll("Manager", 80);
+        company.hireAll("TopManager", 10);
+        System.out.println("Найм списка сотрудников:");
         company.show();
         company.fire();
-        company.sortSalaryTop();
+        System.out.println("Увольнение половины сотрудников");
         company.show();
-        company.getLowestSalaryStaff(counter);
+        company.getTopSalaryStuff(count);
+        company.getLowestSalaryStaff(count);
+
     }
 }
