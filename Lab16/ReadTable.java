94d319eb59ef5d367c0a632b18186449d01f6b35
antonlyn77 <lyndin2001@mail.ru>
2020-12-14 16:29:38 +0300

    Create ReadTable.java
    INIT

---
+++ Lab16/ReadTable.java
@@ -0,0 +1,16 @@
+import org.jsoup.Jsoup;
+
+public class ReadTable {
+    void get(String a) {
+        System.out.println(a);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        ReadTable readTable = new ReadTable();
+        readTable.get("jfgjdhfjkgbfj");
+    }
+}
+
+
