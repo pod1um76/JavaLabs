ba743cc3437295ce11da46693fc1be67cb9bc5d6
antonlyn77 <lyndin2001@mail.ru>
2020-12-18 12:59:58 +0300

    Create Account.java
    INIT

---
+++ Lab19/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    public long money;
+    private String accNumber;
+
+    public Account(long money, String accNumber) {
+        this.money = money;
+        this.accNumber = accNumber;
+    }
+}
